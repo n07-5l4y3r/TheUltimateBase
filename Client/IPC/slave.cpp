796b0c68f718a1c013f693b2b62406d310aa5d33
n075l4y3r <abuse@duck.off>
2020-08-22 18:37:27 +0200

    -

--- Client/IPC/slave.cpp
+++ Client/IPC/slave.cpp
@@ -1,22 +1,37 @@
 #include "slave.hpp"
 
-unsigned __int32 QuerryCB(unsigned __int64 ui64pPacket)
+unsigned __int32 cSlave::QuerryCB(unsigned __int64 ui64pPacket)
 {
 	auto pPacket = (psPacket)ui64pPacket;
+	
+	pPacket->ui64pAfter = cSlave::Init()->ui64pQueueBegin;
+	cSlave::Init()->ui64pQueueBegin = cSlave::Init()->ui64pQueueEnd;
 
-	//pPacket->ui64pAfter = queue begin
 	return 0;
 }
 
-unsigned __int32 PromiseCB(unsigned __int64 ui64pPacket)
+unsigned __int32 cSlave::PromiseCB(unsigned __int64 ui64pPacket)
 {
-	//free ui64pPacket
+	cSlave::Init()->mPacketQueue.at(ui64pPacket).set_value(ui64pPacket);
+	cSlave::Init()->mPacketQueue.erase(ui64pPacket);
 	return 0;
 }
+
+cSlave* cSlave::pInst = nullptr;
 
 cSlave::cSlave() {}
+
+std::future<unsigned __int64> cSlave::queue_cmd(psPacket pPacket)
+{
+	this->ui64pQueueEnd = ((psPacket)this->ui64pQueueEnd)->ui64pAfter = (unsigned __int64)pPacket;
+	this->mPacketQueue.insert(std::make_pair(this->ui64pQueueEnd, std::promise<unsigned __int64>()));
+	return this->mPacketQueue.at(this->ui64pQueueEnd).get_future();
+}
 
 cSlave* cSlave::Init()
 {
 	return cSlave::pInst ? cSlave::pInst : cSlave::pInst = new cSlave();
 }
+
+extern "C" __declspec(dllexport) unsigned __int32 (*QuerryCB)(unsigned __int64 ui64pPacket) = cSlave::QuerryCB;
+extern "C" __declspec(dllexport) unsigned __int32 (*PromiseCB)(unsigned __int64 ui64pPacket) = cSlave::PromiseCB;
--- Client/IPC/slave.hpp
+++ Client/IPC/slave.hpp
@@ -5,14 +5,19 @@
 #include <map>
 #include <future>
 
-extern "C" unsigned __int32 QuerryCB(unsigned __int64 ui64pPacket);
-extern "C" unsigned __int32 PromiseCB(unsigned __int64 ui64pPacket);
-
 class cSlave {
 	static cSlave* pInst;
-	static std::map<unsigned __int64 /*ui64pPacket*/, std::promise<unsigned __int64 /*ui64pPacketResponse*/>> mPacketQueue;
+	//
+	std::map<unsigned __int64 /*ui64pPacket*/, std::promise<unsigned __int64 /*ui64pPacketResponse*/>> mPacketQueue;
+	//
+	unsigned __int64 ui64pQueueBegin = 0ui64, ui64pQueueEnd = 0ui64;
 private:
 	cSlave();
 public:
+	std::future<unsigned __int64> queue_cmd(psPacket pPacket);
+public:
 	static cSlave* Init();
+public:
+	static unsigned __int32 QuerryCB(unsigned __int64 ui64pPacket);
+	static unsigned __int32 PromiseCB(unsigned __int64 ui64pPacket);
 };
